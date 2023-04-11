0ef822d73d37dac0e21c4c900e78c061f8766afc
raul <jarquinr121@gmail.com>
2023-04-10 23:31:23 -0700

    Add movie&rating to local and display movies added from local

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	import MovieInput from './movie.svelte';
+	import MovieList from './movieList.svelte';
+
+	let movies = localStorage.getItem('movies') ?
+	JSON.parse(localStorage.getItem('movies')) :
+	[];
+
+	const submitMovie = movie =>{
+		console.log(movie);
+		const updatedMovies = [...movies, movie]	//all movies w the new movie 
+		localStorage.setItem('movies', JSON.stringify(updatedMovies));	//set updates movies to local
+		movies = updatedMovies;		// 
+	}
+</script>
+
+<main>
+	<div class="main">
+		<h1>Movie Journal</h1>
+		<MovieInput on:submitMovie={event => submitMovie(event.detail.movie)}/>
+		<MovieList movies={movies}/>
+	</div>
+</main>
+
+<style>
+	.main{
+
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/movie.svelte
@@ -0,0 +1,45 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+    const dispatch = createEventDispatcher();
+    let title = '';
+    let rating = 1;
+    
+    const updateMoveTitle = (e) => {       //accept the event from on:keyup
+        title = e.target.value;       //retrieve the current value of an input field with the id or name of "title" when an input change event is triggered
+    }
+    
+    const onRatingSelect = (e) =>{
+        rating = e.target.value;
+    }
+
+    const SubmitMovie = () =>{
+        if(title){  //some how user entered sum in DOM
+            dispatch('submitMovie',{
+                movie:{
+                    title,
+                    rating,
+                }
+            });
+            title='';
+            rating =1;
+        }
+    }
+</script>
+
+<input 
+placeholder="Move Title" 
+type='text'
+value={title}
+on:keyup={updateMoveTitle}     
+/>
+
+
+<select value={rating} on:change={onRatingSelect}>
+    <option value={1}>1</option>
+    <option value={2}>2</option>
+    <option value={3}>3</option>
+    <option value={4}>4</option>
+    <option value={5}>5</option>
+</select>
+
+<button disabled={!title} on:click={SubmitMovie}>Submit</button>
